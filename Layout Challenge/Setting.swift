9d2d3c91b0ddc5ad4bae4fe877d9c72f8395e623
chelsea <kyuhyun0808@gmail.com>
2019-06-17 12:17:40 +0800

    add main storyboard

---
+++ Layout Challenge/Setting.swift
@@ -0,0 +1,9 @@
+//
+//  Setting.swift
+//  Layout Challenge
+//
+//  Created by Chelsea Lin on 2019/6/17.
+//  Copyright © 2019 chelsea lin. All rights reserved.
+//
+
+import Foundation
---
+++ Layout Challenge/SettingTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  SettingTableViewCell.swift
+//  Layout Challenge
+//
+//  Created by Chelsea Lin on 2019/6/17.
+//  Copyright © 2019 chelsea lin. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var iconimage: UIImageView!
+    @IBOutlet weak var settingLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
